6c0b5221e2bea3ce112af4fdf18690572b867d53
Ryan Hantak <rhantak@gmail.com>
2020-03-03 23:19:53 -0700

    Add query parameter to get olympians for youngest olympian

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::OlympiansController < ApplicationController
   def index
-    render json: OlympianSerializer.new(Olympian.all).json_response
+    if request.query_parameters['age'] == "youngest"
+      render json: OlympianSerializer.new(Olympian.youngest).json_response
+    else
+      render json: OlympianSerializer.new(Olympian.all).json_response
+    end
   end
 end
