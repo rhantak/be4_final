f401b5c22d7bc18241306235b27b587fc5eed595
Ryan Hantak <rhantak@gmail.com>
2020-03-03 22:45:37 -0700

    Add method to calculate total medals won

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -1,4 +1,8 @@
 class Olympian < ApplicationRecord
   validates_presence_of :name, :sex, :age, :height, :weight, :team, :sport
   has_many :events
+
+  def total_medals_won
+    events.where.not(medal: "NA").count
+  end
 end
